84115df46489973d0f225b84c2a129b4b3fa1877
Techthoth <olusanyatomiwadavid@gmail.com>
2024-02-26 00:42:56 -0800

    progress

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -5,7 +5,7 @@ import "@fontsource/noto-sans";
 
 const Logo = () => {
   return (
-    <div className='container flex items-center md:p-5 justify-start w-1/2'>
+    <div className='flex items-center md:p-5 justify-start w-1/2'>
     <img src={logo} alt='logo' className='w-[70px] h-[70px] rounded-full'/>
      <span className='font-bold text-sm font-sans text-logo-color md:text-2xl'>Trade Ace</span>
     </div>
