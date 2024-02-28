d0b545d60d66a442cfe598cb763a8cd405b7ad39
Techthoth <olusanyatomiwadavid@gmail.com>
2024-02-28 15:23:33 -0800

    saving progress

--- src/components/Difference.js
+++ src/components/Difference.js
@@ -11,53 +11,21 @@ const Difference = () => {
       <section className='flex flex-col items-start justify-center w-full mb-6 md:w-4/6'>
         <h1 className='md:w-4/5 w-full text-left text-2xl font-bold font-sans text-logo-color p-2 md:text-5xl md:p-8'>What makes Trade Ace different?</h1>
         <div className='flex flex-col w-full gap-1 items-center justify-center'>
-        <ListComponent 
-          iconName="SettingsIcon"
-          listTitlle="tick market data"
-          listDescrion='instant order book update with each change'
-        />
-          <div className='flex w-full items-center justify-start py-5 md:px-5 md:py-3'>
-            <div className='w-[40px] h-[40px] rounded-full flex items-center justify-center bg-logo-color mr-1 md:mr-3'>
-              <SettingsIcon
-                style={{
-                  color: "#fff",
-                  fontSize: '20px'
-                }}
-              />
-            </div>
-            <div className='flex flex-col pl-3 md:pl-6 items-start justify-center'>
-              <h4 className='font-bold text-lg capitalize text-logo-color'>tick market data</h4>
-              <p className='mt-[10px] capitalize font-bold text-xs'>instant order book update with each change</p>
-            </div>
-          </div>
-          <div className='flex w-full items-center justify-start py-5 md:px-5 md:py-3'>
-            <div className='w-[40px] h-[40px] rounded-full flex items-center justify-center bg-logo-color mr-1 md:mr-3'>
-              <CachedIcon
-                style={{
-                  color: "#fff",
-                  fontSize: '20px'
-                }}
-              />
-            </div>
-            <div className='flex flex-col pl-3 md:pl-6 items-start justify-center'>
-              <h4 className='font-bold text-lg capitalize text-logo-color'>lossless order modification</h4>
-              <p className='mt-[10px] capitalize font-bold text-xs'>changes to your order won't affect their priority</p>
-            </div>
-          </div>
-          <div className='flex w-full items-center justify-start py-5 md:px-5 md:py-3'>
-            <div className='w-[40px] h-[40px] rounded-full flex items-center justify-center bg-logo-color mr-1 md:mr-3'>
-              <WifiIcon
-                style={{
-                  color: "#fff",
-                  fontSize: '20px'
-                }}
-              />
-            </div>
-            <div className='flex flex-col pl-3 md:pl-6 items-start justify-center'>
-              <h4 className='font-bold text-lg capitalize text-logo-color'>good-till-session orders</h4>
-              <p className='mt-[10px] capitalize font-bold text-xs'>orders recall from the matcher if connection is lost</p>
-            </div>
-          </div>
+          <ListComponent
+            iconName={SettingsIcon}
+            listTitlle="tick market data"
+            listDescrion='instant order book update with each change'
+          />
+          <ListComponent
+            iconName={CachedIcon}
+            listTitlle="lossless order modification"
+            listDescrion="changes to your order won't affect their priority"
+          />
+          <ListComponent
+            iconName={WifiIcon}
+            listTitlle="good-till-session orders"
+            listDescrion='orders recall from the matcher if connection is lost'
+          />
         </div>
       </section>
       <aside className='flex items-center justify-center h-[500px] w-full md:w-2/6'>
--- src/components/Easy.js
+++ src/components/Easy.js
@@ -1,31 +1,22 @@
 import React from 'react'
+import ListComponent from './ListComponent'
 
 const Easy = () => {
   return (
-    <section className='px-[10vw] py-[5vw] flex items-center justify-center w-full'>
+    <section className='px-[10vw] py-[5vw] flex flex-col items-center justify-center w-full'>
       <h2 className='w-full text-left font-bold font-sans text-4xl capitalize p-5 text-logo-color'>easy start with trade ace</h2>
       <ol>
         <li>
-            
+        <ListComponent
+            listTitlle="tick market data"
+            listDescrion='instant order book update with each change'
+          />
         </li>
-        <li>
-            
-        </li>
-        <li>
-            
-        </li>
-        <li>
-            
-        </li>
-        <li>
-            
-        </li>
-        <li>
-            
-        </li>
-        <li>
-            
-        </li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
       </ol>
     </section>
   )
