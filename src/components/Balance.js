6325fa702533f99c4c90b53c82a84af3bedebd1b
Techthoth <olusanyatomiwadavid@gmail.com>
2024-03-21 17:09:32 -0700

    savivng progress

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Balance = () => {
   return (
-    <div>
+    <div className='w-full py-5 bg-neon-blue mb-10'>
     </div>
   )
 }
--- src/components/Dashmain.js
+++ src/components/Dashmain.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import Barchart from './Barchart'
 import Analytics from './Analytics'
 import Balance from './Balance'
 
 const Dashmain = () => {
   return (
-    <main className='flex flex-col w-full h-fit mt-[125px] px-[20px] py-[20px] items-center justify-center overflow-auto'>
+    <main className='flex flex-col w-full h-fit mt-[100px] md:mt-[180px] px-[10px] md:px-[20px] py-[20px] items-center justify-center overflow-auto'>
       <Balance />
       <Analytics />
     </main>
