a03dd7a0b6ed5395862c420b407ab0e3a65db769
Techthoth <ayotomiwadavid@proton.me>
2024-03-29 07:47:42 -0700

    saving progress

---
+++ src/components/Accountdetails.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Accountdetails = () => {
+  return (
+    <div className='w-[75%] h-[70%] mt-[100px] rounded-md bg-overallBg flex flex-col items-center justify-center p-[10px]'>
+     <main className='flex w-full h-full items-center justify-between'>
+        <div>
+            <img src='' alt='' />
+            <p></p>
+        </div>
+     </main>
+    </div>
+  )
+}
+
+export default Accountdetails
--- src/components/Despositmain.js
+++ src/components/Despositmain.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Depositsidebar from './Depositsidebar'
+import Accountdetails from './Accountdetails'
 
 const Despositmain = () => {
     return (
-        <main className='w-full flex px-10 h-[100%] bg-red-800 items-center justify-center'>
+        <main className='w-full flex px-[10vw] gap-5 h-[100%] items-center justify-between'>
             <Depositsidebar />
+            <Accountdetails />
         </main>
     )
 }
