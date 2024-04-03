fb8d50192453a6a580780d9f5db93d8befc53667
Techthoth <ayotomiwadavid@proton.me>
2024-04-03 16:10:52 -0700

    saving progress

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState} from 'react'
 import Input from './InputComponent';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import HttpsIcon from '@mui/icons-material/Https';
@@ -12,27 +12,6 @@ import { getDatabase, ref, set, push,  get } from 'firebase/database'
 
 const Form = () => {
   const navigate = useNavigate()
-  // let [user, setUser] = useState("");
-  // let readUserData = async (email) => {
-  //   var atIndex = email.indexOf("@");
-  //   var newUserEmail = email.substring(0, atIndex);
-  //   const database = getDatabase(app);
-  //   const databaseRef = ref(database, `users/${newUserEmail}`);
-  //   const snapshot = await get(databaseRef);
-  //   if (snapshot.exists()) {
-  //     let firstuserName = Object.values(snapshot.val());
-  //     let realName = firstuserName[0].userName;
-  //     var atRealNameSpaceIndex = realName.indexOf(' ');
-  //     var userName = realName.substring(0, atRealNameSpaceIndex);
-  //     setUser(userName)
-  //   }
-  // };
-  // useEffect(() => {
-  //   auth.onAuthStateChanged((userCred) => {
-  //     let acountUser = userCred.email;
-  //     readUserData(acountUser);
-  //   });
-  // }, []);
 
   let [visibility, setVisibility] = useState(false)
   let { status } = useParams();
@@ -87,7 +66,6 @@ const Form = () => {
 
 
   let readUserData = async (user) => {
-    console.log(user)
       var atIndex = user.indexOf("@");
       var newUserEmail = user.substring(0, atIndex);
       const database = getDatabase(app);
@@ -98,14 +76,14 @@ const Form = () => {
         let realName = firstuserName[0].userName;
         var atRealNameSpaceIndex = realName.indexOf(' ');
         var userName = realName.substring(0, atRealNameSpaceIndex);
-        navigate(`/${userName}/dashboard`, { replace: true })
+        
       }
+      return navigate(`/${userName}/dashboard`, { replace: true })
     };
   let signIn = async () => {
     await signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
          const user = userCredential.user.email;
-         console.log(user)
          readUserData(user)
         toast.success("You're have been signed in successfull")
       })
