fe11fbbb12da053e2157ced51589823189e64fb2
Techthoth <ayotomiwadavid@proton.me>
2024-04-05 18:16:33 -0700

    saving progress

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -3,7 +3,7 @@ import { getAuth } from 'firebase/auth'
 import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC5zti3r-YRdvYexvSGK5zaXlyV9zou_ns",
+    apiKey: "6LcMFKwpAAAAAGXDxkeQ56PPdRFNG1z4iML0WrBu",
     authDomain: "tradeace-backend-95e84.firebaseapp.com",
     databaseURL: "https://tradeace-backend-95e84-default-rtdb.firebaseio.com",
     projectId: "tradeace-backend-95e84",
@@ -12,11 +12,12 @@ const firebaseConfig = {
     appId: "1:722664596397:web:52d69cd1f292d035147724"
 };
 
-const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
+const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+
 const auth = getAuth(app);
 
 const appCheck = initializeAppCheck(app, {
-  provider: new ReCaptchaV3Provider('6LcMFKwpAAAAAGXDxkeQ56PPdRFNG1z4iML0WrBu'),
+  provider: new ReCaptchaV3Provider('6LcMFKwpAAAAAMa73ejFU7KTcexw3D6vQqXK7VGE'),
 
   // Optional argument. If true, the SDK automatically refreshes App Check
   // tokens as needed.
