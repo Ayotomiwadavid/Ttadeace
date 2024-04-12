11bef22a8fc0a17e815c31c25e9aebae3a9cd0af
Techthoth <ayotomiwadavid@proton.me>
2024-04-12 11:56:42 -0700

    saving progress

--- src/components/Mobilenavbar.js
+++ src/components/Mobilenavbar.js
@@ -4,23 +4,28 @@ import home from '../images/homepng.png'
 import giftcard from '../images/credit-cardpng.png'
 import transaction from '../images/receiptpng.png'
 import logOut from '../images/exitpng.png'
+import { Link } from 'react-router-dom'
 
 const Mobilenavbar = () => {
   return (
     <div className='flex items-center justify-center absolute bottom-0 right-0 left-0 bg-white md:hidden z-10 w-full py-3'>
-      <Mobilenav 
-        imageSrc={home}
-        navName='home'
-      />
-      <Mobilenav 
-        imageSrc={giftcard}
-        navName='giftcards'
-      />
-      <Mobilenav 
+      <Link to='/dashboard' >
+        <Mobilenav
+          imageSrc={home}
+          navName='home'
+        />
+      </Link>
+      <Link to='/withdraw'>
+        <Mobilenav
+          imageSrc={giftcard}
+          navName='withdraw'
+        />
+      </Link>
+      <Mobilenav
         imageSrc={transaction}
         navName='transactions'
       />
-      <Mobilenav 
+      <Mobilenav
         imageSrc={logOut}
         navName='log out'
       />
